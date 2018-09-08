f07bb8e0e1e3ea0a44f179e09e1aef9b2f4e2c63
GaoHeming111 <651812464@qq.com>
2018-09-08 11:33:56 +0800

    算法

--- VUE2/post.php
+++ VUE2/post.php
@@ -1,5 +1,15 @@
 <?
-    $a=$_POST['a'];
-    $b=$_POST['b'];
-    echo $a-$b;
+	header("Content-Type:text/html;charset=utf-8");
+	header("Access-Control-Allow-Origin:*");
+	$str = <<<str [{
+		"name":"gao",
+		"age":"12"
+	}]
+//  $a=$_POST['a'];
+//  $b=$_POST['b'];
+//  echo $a-$b;
+//  $name = $_POST['name'];
+//  $age = $_POST['age'];
+//  echo $name.$age;
+	echo $str;
 ?>
